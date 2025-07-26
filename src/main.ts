04ab34504248b3ae1f9eb5f0cba4d8691516c454
Aram2712 <aram271290@mail.ru>
2025-07-27 01:26:02 +0400

    secure add

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import * as fs from 'fs';
+import * as fs from 'fs';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as cookieParser from 'cookie-parser';
@@ -10,25 +10,30 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   secureProtocol: 'TLS_method',
-  //   ciphers: [
-  //     'ECDHE-ECDSA-AES128-GCM-SHA256',
-  //     'ECDHE-RSA-AES128-GCM-SHA256',
-  //     'ECDHE-ECDSA-AES256-GCM-SHA384',
-  //     'ECDHE-RSA-AES256-GCM-SHA384',
-  //     'ECDHE-ECDSA-CHACHA20-POLY1305',
-  //     'ECDHE-RSA-CHACHA20-POLY1305',
-  //     'DHE-RSA-AES128-GCM-SHA256',
-  //     'DHE-RSA-AES256-GCM-SHA384',
-  //     'TLS_AES_256_GCM_SHA384',
-  //     'TLS_CHACHA20_POLY1305_SHA256',
-  //     'TLS_AES_128_GCM_SHA256',
-  //   ].join(':'),
-  //   honorCipherOrder: true,
-  // };
+  // let process;
+  const httpsOptions = {
+    key: '/etc/letsencrypt/live/ukladka-plitki.ru/privkey.pem)',
+    cert: '/etc/letsencrypt/live/ukladka-plitki.ru/fullchain.pem',
+    secureProtocol: 'TLS_method',
+    ciphers: [
+      'ECDHE-ECDSA-AES128-GCM-SHA256',
+      'ECDHE-RSA-AES128-GCM-SHA256',
+      'ECDHE-ECDSA-AES256-GCM-SHA384',
+      'ECDHE-RSA-AES256-GCM-SHA384',
+      'ECDHE-ECDSA-CHACHA20-POLY1305',
+      'ECDHE-RSA-CHACHA20-POLY1305',
+      'DHE-RSA-AES128-GCM-SHA256',
+      'DHE-RSA-AES256-GCM-SHA384',
+      'TLS_AES_256_GCM_SHA384',
+      'TLS_CHACHA20_POLY1305_SHA256',
+      'TLS_AES_128_GCM_SHA256',
+    ].join(':'),
+    honorCipherOrder: true,
+  };
 
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    httpsOptions
+  });
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
