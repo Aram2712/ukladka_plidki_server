6431a5edfd9b57f51aa900a8f9382e1fa148b45d
Aram2712 <aram271290@mail.ru>
2025-07-29 07:53:31 +0400

    change useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
 import { OrdersModule } from './orders/orders.module';
 import { MessageModule } from './messages/message.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import {TypeOrmModule, TypeOrmModuleOptions} from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CommentsModule } from './comments/comments.module';
 import { ForumModule } from './forum/forum.module';
@@ -18,30 +18,20 @@ import { PriceListModule } from './priceList/priceList.module';
       // envFilePath: `.env.development`,
       envFilePath: `.env.production`,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      // useFactory: (config: ConfigService) => ({
-      //     type: 'mysql' as const,
-      //     host: config.get<string>('DB_HOST') ?? 'localhost',
-      //     port: Number(config.get<string>('DB_PORT')) || 3306,
-      //     username: config.get<string>('DB_USERNAME') ?? 'root',
-      //     password: config.get<string>('DB_PASSWORD') ?? 'root',
-      //     database: config.get<string>('DB_NAME') ?? 'uklad_plidka',
-      //     autoLoadEntities: true,
-      //     synchronize: true, // ⚠️ только для разработки!
-      //   })
-        useFactory: () => ({
-            type: 'mysql' as const,
-            host: 'localhost',
-            port: 3306,
-            username: 'plidkaroot',
-            password: 'plidkapass',
-            database: 'ukladplidki',
-            autoLoadEntities: true,
-            synchronize: true, // ⚠️ только для разработки!
-        })
-    }),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
+              type: 'mysql',
+              host: config.get<string>('DB_HOST'),
+              port: Number(config.get<string>('DB_PORT')) || 3306,
+              username: config.get<string>('DB_USERNAME'),
+              password: config.get<string>('DB_PASSWORD'),
+              database: config.get<string>('DB_NAME'),
+              autoLoadEntities: true,
+              synchronize: true, // ⚠️ отключи в проде позже!
+          }),
+      }),
     AuthModule,
     ServicesModule,
     OrdersModule,
