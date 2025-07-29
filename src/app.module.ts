ab8a766d39216352485e5b1193992479bfbb67f6
Aram2712 <aram271290@mail.ru>
2025-07-29 08:19:48 +0400

    change eli

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,11 @@ import { PriceListModule } from './priceList/priceList.module';
           inject: [ConfigService],
           useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
               type: 'mysql',
-              host: config.get<string>('DB_HOST'),
-              port: Number(config.get<string>('DB_PORT')) || 3306,
-              username: config.get<string>('DB_USERNAME'),
-              password: config.get<string>('DB_PASSWORD'),
-              database: config.get<string>('DB_NAME'),
+              host: 'localhost',
+              port: 3306,
+              username: 'plidkaroot',
+              password: 'plidkapass',
+              database: 'ukladplidki',
               autoLoadEntities: true,
               synchronize: true, // ⚠️ отключи в проде позже!
           }),
